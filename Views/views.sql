eecb972487d99fdd2e5dc27501fdc6d93caf71e3
sharath23 <ravichandran.sh@northeastern.edu>
2022-04-27 16:27:02 -0400

    Adding customer views

--- Views/views.sql
+++ Views/views.sql
@@ -8,3 +8,13 @@ case
     else 'Affordable'
 end as Division
 from item;
+
+----Customer order History
+Create or replace view customer_order_history as
+SELECT c.cust_id, c.cust_name, o.order_id,p.Amount, o.order_date
+FROM Customer c
+INNER JOIN Orderr o ON c.cust_id = o.cust_id
+INNER JOIN PAYMENT p ON p.ORDER_ID = o.order_id
+ORDER BY o.order_date;
+
+select * from customer_order_history;
