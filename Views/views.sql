27a9e8f069051fee72216814dba2567fecad7210
sharath23 <ravichandran.sh@northeastern.edu>
2022-04-27 16:28:40 -0400

    Adding shipping views

--- Views/views.sql
+++ Views/views.sql
@@ -18,3 +18,13 @@ INNER JOIN PAYMENT p ON p.ORDER_ID = o.order_id
 ORDER BY o.order_date;
 
 select * from customer_order_history;
+
+
+------Shipping status summary
+Create or replace view customer_order_status as
+SELECT o.ORDER_ID, c.Cust_ID, c.Cust_Name, s.SHIPMENT_ID, s.DELIVERY_STATUS, s.SHIPMENT_DATE, o.ORDER_DATE, sh.shipper_name
+from CUSTOMER c
+inner join ORDERR o on c.Cust_ID = o.Cust_ID
+inner join PAYMENT p on p.ORDER_ID = o.ORDER_ID
+inner join SHIPMENT s on s.PAYMENT_ID = P.PAYMENT_ID
+inner join shipper sh on s.SHIPPER_ID = sh.SHIPPER_ID;
