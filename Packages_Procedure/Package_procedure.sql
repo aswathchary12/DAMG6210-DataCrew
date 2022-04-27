202ca5787f1e2074ceac3b20d8de93142f131132
csuryasubbiah <cheranthian.s@northeastern.edu>
2022-04-27 14:12:21 -0400

    adding Procedure Price Based

--- Packages_Procedure/Package_procedure.sql
+++ Packages_Procedure/Package_procedure.sql
@@ -29,3 +29,33 @@ EXCEPTION
         WHEN no_data_found THEN
         dbms_output.put_line('Items does not exist');
 END Based_On_Category;
+
+--Displaying items based on the ranges of price
+
+PROCEDURE Price_Based(p in number) IS
+    itm_price item.price%type;
+    itm_name item.item_name%type;
+    itm_id item.item_id%type;
+    CURSOR cur1 IS
+    SELECT item_id,price,item_name FROM item WHERE price < p;
+    BEGIN
+        OPEN cur1;
+        LOOP
+            FETCH cur1 INTO itm_id,itm_price,itm_name;
+            EXIT WHEN cur1%notfound;
+            dbms_output.put_line('The Item ID: ' || itm_id || ' The Item name: "' || itm_name || '" and its price is ' || itm_price);
+        END LOOP;
+        CLOSE cur1;
+    EXCEPTION
+        WHEN no_data_found THEN
+        dbms_output.put_line('No such item exist');
+    END Price_Based;
+
+END;
+/
+
+
+EXECUTE ITEM_PACKAGE.Based_On_Category('FURNITURE');
+EXECUTE ITEM_PACKAGE.Price_Based(50);
+
+----------------------------------------------------------------------------------------
