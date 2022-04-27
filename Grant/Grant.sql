7f1b8671628eb62694b27fa9f7106d9860445ae3
Janani Balaji <balaji.j@northeastern.edu>
2022-04-27 17:09:11 -0400

    Grant-grant create session

--- Grant/Grant.sql
+++ Grant/Grant.sql
@@ -3,3 +3,7 @@
 CREATE USER Customer1 IDENTIFIED BY Oracle_12345;
 CREATE USER Seller1 IDENTIFIED BY Oracle_12345;
 CREATE USER Shipper1 IDENTIFIED BY Oracle_12345;
+
+grant create session to Customer1;
+grant create session to Seller1;
+grant create session to Shipper1;
