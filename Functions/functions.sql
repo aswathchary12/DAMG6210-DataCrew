0e988a4e77228fe43e95f53eeb35dcbf9c963aef
sharath23 <ravichandran.sh@northeastern.edu>
2022-04-27 16:17:02 -0400

    Adding Functions-order time

--- Functions/functions.sql
+++ Functions/functions.sql
@@ -19,3 +19,18 @@ c := totalProducts(6002);
 DBMS_output.put_line('Total number of items the seller sells is : '|| c);
 end;
 /
+
+--To get Order Time
+CREATE OR REPLACE FUNCTION Get_Order_Time (Paym_ID NUMBER)
+RETURN NUMBER
+AS
+ v_temp NUMBER(10);
+BEGIN
+SELECT  s.SHIPMENT_DATE- p.PAYMENT_Date INTO v_temp
+FROM SHIPMENT s
+INNER JOIN PAYMENT p
+on s.PAYMENT_ID = p.PAYMENT_ID
+where s.PAYMENT_ID = Paym_ID;
+RETURN v_temp;
+END;
+/
